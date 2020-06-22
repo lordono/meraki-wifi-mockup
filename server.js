0d9669fc3ea941e95450503de57c35d5493526bb
Jun Liang <seowjunliang@gmail.com>
2020-06-22 19:18:56 +0800

    added server.js to serve

---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const path = require("path");
+const app = express();
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(process.env.PORT || 5555);
